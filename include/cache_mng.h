1d29b9ea0dcaf1535237dd37d17eea6faed58eba
wizzard <wizzard@burna>
2012-09-23 01:10:03 +0200

    cleaning the code, renamed files

--- include/cache_mng.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CACHE_MNG_H_
-#define _CACHE_MNG_H_
-
-#include "include/global.h"
-
-CacheMng *cache_mng_create (Application *app, const gchar *path);
-void cache_mng_destroy (CacheMng *mng);
-
-#endif

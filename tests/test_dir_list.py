9991da1bd0d97bc29bef5b2b1ca8705973cd2e2b
meinharrd <meinhard@benn.org>
2013-02-13 01:28:24 +0900

    compare thread results

--- tests/test_dir_list.py
+++ tests/test_dir_list.py
@@ -6,6 +6,9 @@ import time
 if len(sys.argv) < 3:
     sys.exit("Usage: {} [num threads] [path]".format(sys.argv[0]))
 
+dirs_list = []
+files_list = []
+
 class ThreadClass(threading.Thread):
     def run(self):
         total_files = 0
@@ -15,7 +18,8 @@ class ThreadClass(threading.Thread):
                 total_dirs += 1
             for filename in filenames:
                 total_files += 1
-        print "dirs: {}, files: {}".format(total_dirs, total_files)
+        dirs_list.append(total_dirs)
+        files_list.append(total_files)
 
 class Main():
     def runit(self):
@@ -29,6 +33,12 @@ class Main():
 
         for item in t_list:
             t.join()
+
+        # test if directory and file counters are the same for all threads
+        if (len(set(dirs_list)) == 1 and len(set(files_list)) == 1):
+            print "OK: dirs: {}, files: {}".format(dirs_list[0], files_list[0])
+        else:
+            print "ERROR: Not all directory and file counters are equal"
 
 
 m = Main()

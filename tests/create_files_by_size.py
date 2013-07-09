db9ca967a49c1da94554d788f8626231db7706ea
Paul Ionkin <paul.ionkin@gmail.com>
2013-07-09 09:54:14 +0200

    Improved script

--- tests/create_files_by_size.py
+++ tests/create_files_by_size.py
@@ -22,19 +22,23 @@ def read_file (fname):
     fin = open (fname, 'r')
     x = fin.read ()
     fin.close ()
-    print fname + " size: " + str (len (x))
 
 def create_files (dir, nr_files):
     for i in range (1, nr_files + 1):
         fname = dir + "/" + str_gen () + "_" + str (i)
+        print "<<< " + fname
         create_file (fname, i)
         l_files.append (fname)
 
 def read_files ():
     for l in l_files:
+        print ">>> " + l
         read_file (l)
 
-
 if __name__ == "__main__":
-    create_files (sys.argv[1], 100)
+    if len (sys.argv) < 3:
+        print "Please run as: " + sys.argv[0] + " [outdir] [file num]"
+        sys.exit (1)
+
+    create_files (sys.argv[1], int (sys.argv[2]))
     read_files ()
